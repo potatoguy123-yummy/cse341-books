b72c9877d18f13ceb36a7e21e2ec0c9ad87eef29
Ethan O'Brien <ethan.a.obrienw@gmail.com>
2026-09-08 10:11:48 -0700

    Add swagger documentation

--- src/router.js
+++ src/router.js
@@ -3,7 +3,65 @@ import { getBooksHandler, getBookByIdHandler } from "./controllers/books.js";
 
 const router = express.Router();
 
+/**
+ * @openapi
+ * /books:
+ *   get:
+ *     summary: Get all books
+ *     tags:
+ *       - Books
+ *     responses:
+ *       200:
+ *         description: Books returned successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: "#/components/schemas/Book"
+ *       500:
+ *         description: Unable to retrieve books
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Error"
+ */
 router.get("/books", getBooksHandler);
+
+/**
+ * @openapi
+ * /books/{id}:
+ *   get:
+ *     summary: Get one book by id
+ *     tags:
+ *       - Books
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The custom book id, such as b1
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Book returned successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Book"
+ *       404:
+ *         description: Book not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Error"
+ *       500:
+ *         description: Unable to retrieve book
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Error"
+ */
 router.get("/books/:id", getBookByIdHandler);
 
 export default router;
---
+++ src/swagger-schemas.js
@@ -0,0 +1,27 @@
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Book:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           example: b1
+ *         author:
+ *           type: string
+ *           example: Maya Rivera
+ *         title:
+ *           type: string
+ *           example: Patterns of Light
+ *         publicationDate:
+ *           type: string
+ *           example: "2021-08-17"
+ *     Error:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ *           example: Internal server error
+ */
+export {};
